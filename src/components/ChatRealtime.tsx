41478bf2fb5afedc0f142e8e6bc4a8b860471745
Krzysztof <xainoofficial@gmail.com>
2025-10-30 18:34:42 +0100

    refactor: Remove unnecessary React import in ChatRealtime and AI pages

--- src/components/ChatRealtime.tsx
+++ src/components/ChatRealtime.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useAuth } from "../auth/AuthProvider";
 import Pusher from "pusher-js";
 import { useTranslation } from "../i18n";
--- src/pages/AI.tsx
+++ src/pages/AI.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import PageHeader from "../components/PageHeader";
 import AnimalClassifier from "../components/AnimalClassifier";
 import ChatRealtime from "../components/ChatRealtime";
